9a0705778f3bfb1fb11ae8366e02cf554eff4e7b
Tabitha Rollins <tabitha@keyneston.com>
2019-12-10 17:20:34 +0000

    Doesn't work, but start of reading data back

--- src/rbgp/connection.rs
+++ src/rbgp/connection.rs
@@ -1,5 +1,9 @@
 use crate::messages::{Error, Open};
+use std::io::{stdout, Write};
+use std::time::Duration;
+use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
+use tokio::time::delay_for;
 
 pub struct Connection {
     remote: String,
@@ -16,6 +20,15 @@ impl Connection {
             ..Default::default()
         };
         o.write_bytes(&mut stream).await?;
+
+        loop {
+            delay_for(Duration::from_secs(60)).await;
+
+            let mut buf: [u8; 1024] = [0; 1024];
+            stream.read(&mut buf).await?;
+
+            stdout().write(&buf)?;
+        }
 
         Ok(Connection {
             remote: addr.to_string(),
